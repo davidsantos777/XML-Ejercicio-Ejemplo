6dcb2b92b54a158c96ecff718df21495ed66d846
David Santos <35598098+davidsantos777@users.noreply.github.com>
2019-03-25 16:43:23 +0100

    Add files via upload

--- radares.py
+++ radares.py
@@ -6,6 +6,16 @@ def cantidad_radares(doc):
 	lista_radares = doc.xpath("count(//CARRETERA/RADAR)")
 	return lista_radares
 
+def carreteras_radares(doc,provincia):
+	contador = 1
+	lista_carreteras = doc.xpath("//PROVINCIA[NOMBRE='%s']/CARRETERA/DENOMINACION/text()"%(provincia.title()))
+	for i in lista_carreteras:
+			print("Carretera",contador,":",i)
+			contador+=1
+	lista_radares = doc.xpath("count(//PROVINCIA[NOMBRE='%s']//CARRETERA/RADAR)"%(provincia.title()))
+	print("Nº de radares en la provincia de", provincia,":",int(lista_radares))
+
+
 from lxml import etree
 
 doc = etree.parse('radares.xml')
@@ -26,6 +36,10 @@ while True:
 
 	if opcion=="2":
 		print("Cantidad de radares:",int(cantidad_radares(doc)))
+
+	if opcion=="3":
+		provincia = input("Dime el nombre de una provincia: ")
+		print(carreteras_radares(doc,provincia))
 
 	if opcion=="0":
 		break
